d040fa9f2f4b3a0436ead08311a9c4fbeddb0455
Aryan V S <72266394+Lost-Arrow@users.noreply.github.com>
2021-09-05 15:06:14 +0530

    Added __init__.py for clashofclans and renamed clashofclans.py to coc.py

---
+++ additional-cogs/clashofclans/__init__.py
@@ -0,0 +1,14 @@
+from cochttp import (
+    CocHTTPClient
+)
+from clan import (
+    Clan,
+    ClanMembers,
+    ClanWar,
+    ClanWarLog,
+    ClanWarLeagueWar,
+    ClanWarLeagueGroup
+)
+from player import (
+    Player
+)
